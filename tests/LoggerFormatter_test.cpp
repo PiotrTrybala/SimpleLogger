c4e7f6c8f6f3b24be629b7febc341d87afbc1849
Piotr Trybała <trybalapiotr4@gmail.com>
2022-10-02 18:15:46 +0200

    [LoggerFormatter] added testes for LoggerFormatter

---
+++ tests/LoggerFormatter_test.cpp
@@ -0,0 +1,70 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../include/LoggerFormatter.h"
+
+#include "../lib/doctest/doctest/doctest.h"
+
+#include <iostream>
+
+using namespace NFCServer::Logger;
+using namespace std;
+
+const string LoggerName = "SimpleLogger";
+
+TEST_CASE("Simple log without formatting") {
+
+    LoggerFormatter formatter {LoggerName};
+
+    string result = formatter.FormatMessage("Message", LoggerLevel::NONE);
+
+    CHECK(result == "Message\n");
+
+}
+
+TEST_CASE("Simple log with logger level") {
+    LoggerFormatter formatter {LoggerName};
+    string formatString = "[%L] Simple message";
+    string result = formatter.FormatMessage(formatString, LoggerLevel::INFO);
+    CHECK(result == "[\033[1;32mInfo\033[0m] Simple message\n");
+
+    result = formatter.FormatMessage(formatString, LoggerLevel::WARN);
+    CHECK(result == "[\033[1;33mWarn\033[0m] Simple message\n");
+
+    result = formatter.FormatMessage(formatString, LoggerLevel::ERROR);
+    CHECK(result == "[\033[1;31mError\033[0m] Simple message\n");
+
+}
+
+TEST_CASE("Simple log with datetime") {
+
+    LoggerFormatter formatter {LoggerName};
+
+    string formatString = "[%Y:%M:%D] {%H:%m:%S} Simple message";
+
+    time_t test_time = time(nullptr);
+    struct tm* ct = localtime(&test_time);
+
+    auto formatTime = [&](int timeUnit) {
+        std::string out = "";
+        if (timeUnit < 10) out += "0";
+        out += std::to_string(timeUnit);
+        return out;
+    };
+
+    string result = formatter.FormatMessage(formatString, LoggerLevel::NONE);
+
+    string compare_string = "[";
+    compare_string += formatTime(ct->tm_year + 1900) += ":";
+    compare_string += formatTime(ct->tm_mon + 1) += ":";
+    compare_string += formatTime(ct->tm_mday) += "] {";
+
+    compare_string += formatTime(ct->tm_hour) += ":";
+    compare_string += formatTime(ct->tm_min) += ":";
+    compare_string += formatTime(ct->tm_sec) += "} Simple message\n";
+
+    CHECK(compare_string == result);
+
+}
+
+TEST_CASE("Simple log with logger name") {
+
+}
