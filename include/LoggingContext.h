f469e8fef06cd99b51e33915aa78386862f63656
Piotr Trybała <trybalapiotr4@gmail.com>
2022-10-02 18:48:08 +0200

    [LoggingContext] started LoggingContext

---
+++ include/LoggingContext.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Logger.h"
+
+namespace NFCServer {
+    namespace Logger {
+
+        class LoggingContext {
+
+            
+
+        };
+
+    }
+}
