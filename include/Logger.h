77b8554a3088fe8182f00ce36914126b9a0df0f2
Piotr Trybała <trybalapiotr4@gmail.com>
2022-10-08 12:07:40 +0200

    [Logger] added to Logger class option to use LoggingContext

--- include/Logger.h
+++ include/Logger.h
@@ -16,22 +16,32 @@ namespace NFCServer {
                 LoggerFormatter* _formatter;
                 std::ostream& os = std::cout;
 
+                bool _useLoggingContext;
+                LoggingContext* _context;
+
                 void Print(const LoggerLevel& level, const std::string& message, const std::string& format);
             public:
-                Logger(const std::string& _loggerName, const std::string& format = DEFAULT_FORMAT) : _name(_loggerName), _formatter(new LoggerFormatter(_loggerName)) {}
+                Logger(const std::string& _loggerName, const std::string& format = DEFAULT_FORMAT, bool useLoggingContext = false, LoggingContext* ctx = nullptr) : _name(_loggerName), _formatter(new LoggerFormatter(_loggerName)), _useLoggingContext(useLoggingContext), _context(ctx) {}
                 ~Logger() {
+                    delete _context;
                     delete _formatter;
                 }
                 Logger(const Logger& rhs) {
                     _formatter = new LoggerFormatter(rhs._name);
                     *_formatter = *rhs._formatter;
+                    _context = new LoggingContext();
+                    *_context = *rhs._context;
                 }
                 Logger operator=(const Logger& rhs) {
                     if (this == &rhs) return *this;
 
                     _formatter = new LoggerFormatter(rhs._name);
+                    // TODO: check if this assignment is necessary
                     *_formatter = *rhs._formatter;
 
+                    _context = new LoggingContext();
+                    *_context = *rhs._context;
+                    
                     return *this;
                 }
 
